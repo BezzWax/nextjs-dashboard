344cfffc3a9b6409009e10f6f55ff69023ee047d
Beezwax <kolomiec.evgeniy03@gmail.com>
2025-05-14 15:40:41 +0300

    feat: add sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import styles from "@/app/ui/home.module.css";
 
 export default function Page() {
+
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -27,6 +29,13 @@ export default function Page() {
             width={1000}
             height={760}
             className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
             alt="Screenshots of the dashboard project showing desktop version"
           />
         </div>
